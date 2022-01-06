54fa9e5dccdbc7df77f3580a8a78ae6e7d5efa9a
Le Louët <90548369+MrEleffant@users.noreply.github.com>
2022-01-06 11:53:46 +0100

    V1.0

---
+++ CreateDB.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const fs = require('fs')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const db = require('./db.json')
+
+client.login(process.env.TOKEN)
+
+client.on('message', (message) => {
+    const dbChann = '928597107441561610'
+    if(message.channel.id == dbChann) {
+        const content = message.content.split('-')
+        const question = content[0]
+        const response = content[1]
+        if (db[question]) {
+            message.react('❌')
+            return
+        } else {
+            message.react('✅')
+            db[question] = response
+            writeJsonFileUTF8("./db.json", db);
+        }
+        return
+    }
+    return
+})
+
+async function writeJsonFileUTF8(filename, content) {
+    fs.writeFile(filename,
+        JSON.stringify(content, null, 1), "utf8",
+        (err) => {
+            if (err) { console.log(err); }
+        }
+        );
+}
